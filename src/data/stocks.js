a298f135e874a278d336a30a07429b648b659a81
David Valles <david.valles@wizeline.com>
2018-03-10 15:55:18 -0600

    Added Vuex, New statate control for stocks

---
+++ src/data/stocks.js
@@ -0,0 +1,7 @@
+export default [
+    { id: 1, name: 'Apple', price: 1000 },
+    { id: 2, name: 'Amazon', price: 900 },
+    { id: 3, name: 'Google', price: 800 },
+    { id: 4, name: 'Facebook', price: 300 },
+    { id: 5, name: 'Twitter', price: 100 }
+];
